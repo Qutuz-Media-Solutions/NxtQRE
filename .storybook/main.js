e5797266947d6270a9af488b96e51a1826436e7b
Ahmad Kayyali <kayyali18@gmail.com>
2023-01-14 16:05:22 -0500

    docs: add storybook migration guide :book:

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,6 @@
 // https://github.com/storybookjs/storybook/blob/next/code/frameworks/nextjs/README.md#notes-for-yarn-v2-and-v3-users
-
+/* --Storybook Migration Guide-- */
+// https://chromatic-ui.notion.site/Storybook-7-migration-guide-dbf41fa347304eb2a5e9c69b34503937
 const path = require('path');
 module.exports = {
   stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
