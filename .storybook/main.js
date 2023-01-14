d9f5025ae39608f9d77118bf074f48c5dcf114a7
Ahmad Kayyali <kayyali18@gmail.com>
2023-01-14 16:00:12 -0500

    build: add viewports and staticdirs - storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,19 +1,19 @@
+// https://github.com/storybookjs/storybook/blob/next/code/frameworks/nextjs/README.md#notes-for-yarn-v2-and-v3-users
+
 const path = require('path');
 module.exports = {
-  "stories": [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  staticDirs: ['../public'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": {
-    "name": "@storybook/nextjs",
-    "options": {}
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
   },
-  "docs": {
-    "autodocs": "tag"
-  }
-}
+  docs: {
+    autodocs: 'tag',
+  },
+};
