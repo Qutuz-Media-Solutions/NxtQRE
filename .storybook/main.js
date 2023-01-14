f9e9458bdcfbccc919c9e324e543baac7e433fa2
Ahmad Kayyali <kayyali18@gmail.com>
2023-01-14 07:44:40 -0500

    build: setting up storybook :book:

---
+++ .storybook/main.js
@@ -0,0 +1,18 @@
+https://github.com/storybookjs/storybook/tree/next/code/frameworks/nextjs
+
+const path = require('path');
+module.exports = {
+  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+};
