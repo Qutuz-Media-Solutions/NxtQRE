bd25873d96cd58a0f9066c4c439c59bf116ad54f
Ahmad Kayyali <kayyali18@gmail.com>
2023-01-17 23:07:27 -0500

    fix: update type names and import

--- app/components/templates/base-template/index.tsx
+++ app/components/templates/base-template/index.tsx
@@ -1,6 +1,6 @@
 // https://nextjs.org/docs/basic-features/built-in-css-support
 import styles from './index.module.sass';
-import { IBaseTemplate } from './types.dt';
+import { IBaseTemplate } from './types';
 
 const BaseTemplate: React.FC<IBaseTemplate> = ({ sampleTextProp }) => {
   return <div className={styles.container}>{sampleTextProp}</div>;
