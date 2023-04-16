7f7473653b3d61d021cdb592346ddffa8d60e7d0
Ahmad Kayyali <kayyali18@gmail.com>
2023-04-16 15:16:49 -0400

    refactor: remove supabase

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,6 @@ import 'server-only';
 import 'styles/globals.sass';
 
 import PrimaryLayout from '@/layouts/primary/PrimaryLayout';
-import { createServerClient } from 'lib/supabase/clients/server';
-import SupabaseListener from './context/supabase/listener';
-import SupabaseProvider from './context/supabase/provider';
 import Navbar from './layouts/navbar';
 
 //  TODO: Remove 3arabi font import here, only use when needed
@@ -14,21 +11,11 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  // Initialise server singleton
-  const supabase = createServerClient();
-  const {
-    data: { session },
-    error,
-  } = await supabase.auth.getSession();
-
   return (
     <html lang="en" className={`${grandstander.variable} ${arabic.variable}`}>
       <body>
-        <SupabaseProvider session={session} error={error}>
-          <SupabaseListener />
-          <Navbar />
-          <PrimaryLayout>{children}</PrimaryLayout>
-        </SupabaseProvider>
+        <Navbar />
+        <PrimaryLayout>{children}</PrimaryLayout>
       </body>
     </html>
   );
--- lib/supabase/clients/browser.ts
+++
@@ -1,5 +0,0 @@
-import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
-import type { Database } from 'lib/supabase/types/database.types';
-
-export const createBrowserClient = () =>
-  createBrowserSupabaseClient<Database>();
