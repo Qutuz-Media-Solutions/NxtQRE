f1d854735190519ee67ff4b6204957e7c921a99b
Ahmad Kayyali <kayyali18@gmail.com>
2023-04-16 15:25:09 -0400

    refactor: add esseential & upgrade to 13.3

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,24 @@
+import PrimaryLayout from '@/layouts/primary/PrimaryLayout';
 import { arabic, grandstander } from '@/meta/fonts';
+import { Metadata } from 'next';
 import 'server-only';
 import 'styles/globals.sass';
-
-import PrimaryLayout from '@/layouts/primary/PrimaryLayout';
 import Navbar from './layouts/navbar';
+
+export const metadata: Metadata = {
+  applicationName: 'Next.js',
+  description:
+    'Phoenix Technical Industries is an international distributor of Medical Equipment and Supplies',
+  title: 'Phoenix TI',
+  referrer: 'origin-when-cross-origin',
+  keywords: ['Next.js', 'React', 'JavaScript'],
+  authors: [{ name: 'Ahmad K' }, { name: 'Omar Q' }],
+  viewport: { width: 'device-width', initialScale: 1 },
+  icons: ['/favicon.svg'],
+  colorScheme: 'dark',
+  creator: 'Qutuz Media',
+  publisher: 'Qutuz Media',
+};
 
 //  TODO: Remove 3arabi font import here, only use when needed
 export default async function RootLayout({
--- app/meta/fonts.ts
+++ app/meta/fonts.ts
@@ -1,13 +1,13 @@
 // https://beta.nextjs.org/docs/optimizing/fonts
-import { Grandstander, Kufam } from '@next/font/google';
+import { Grandstander, Kufam } from 'next/font/google';
 
 export const grandstander = Grandstander({
-  display: 'optional',
+  display: 'swap',
   variable: '--font-grandstander',
 });
 
 export const arabic = Kufam({
-  display: 'optional',
+  display: 'swap',
   variable: '--font-arabic',
   subsets: ['arabic'],
 });
