8b767ac601ef0c78c383314ca38f174a7da95a81
Ahmad Kayyali <kayyali18@gmail.com>
2023-04-16 15:26:10 -0400

    fix: update fonts subset

--- app/meta/fonts.ts
+++ app/meta/fonts.ts
@@ -4,6 +4,7 @@ import { Grandstander, Kufam } from 'next/font/google';
 export const grandstander = Grandstander({
   display: 'swap',
   variable: '--font-grandstander',
+  subsets: ['latin'],
 });
 
 export const arabic = Kufam({
