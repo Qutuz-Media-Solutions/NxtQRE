9ef9c6dfe39be82d36436357ee350e8477cd6fc5
Omar-AlQalei <113255672+Omar-AlQalei@users.noreply.github.com>
2023-04-28 03:27:29 +0300

    ci: added home and admin pages

---
+++ app/(admin)/page.tsx
@@ -0,0 +1,9 @@
+const Dashboard: React.FC = () => {
+    return (
+        <section className="">
+            <h2>Layout Example (Dashboard) </h2>
+        </section>
+    );
+};
+
+export default Dashboard;
---
+++ app/(home)/page.tsx
@@ -0,0 +1,9 @@
+const Home: React.FC = () => {
+  return (
+    <section className="">
+      <h2>Layout Example (Home) </h2>
+    </section>
+  );
+};
+
+export default Home;
